c59199916e0090a089e16fe21a83202f902dd9b6
ShroukOuda <s36568961@gmail.com>
2025-10-10 15:20:30 +0300

    add countrycode

--- AirBnbCloneAPI/Dtos/RegisterDto.cs
+++ AirBnbCloneAPI/Dtos/RegisterDto.cs
@@ -7,18 +7,31 @@ public class RegisterDto
     [Required]
     [StringLength(50, MinimumLength = 3)]
     public string FirstName { get; set; }
+    
     [Required]
     [StringLength(50, MinimumLength = 3)]
     public string LastName { get; set; }
+    
     [Required]
-    public DateTime BirthDate { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; }
+    
     [Required]
     public string UserName { get; set; }
     
-    public string PhoneNumber { get; set; }
     [Required]
-    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be 8 characters or less")]
+    [StringLength(2)]
+    public string CountryCode { get; set; }
+    
+    [Required]
+    [Phone(ErrorMessage = "Invalid phone number")]
+    public string PhoneNumber { get; set; }
+    
+    [Required]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 100 characters long")]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
+    
     [Required]
     [EmailAddress]
     public string Email { get; set; }
