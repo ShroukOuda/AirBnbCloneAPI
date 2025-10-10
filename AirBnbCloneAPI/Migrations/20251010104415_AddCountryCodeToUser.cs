c4303c8b4710f4be5e9a1458c3aec8ec16d99bbe
ShroukOuda <s36568961@gmail.com>
2025-10-10 15:40:50 +0300

    add migrations

---
+++ AirBnbCloneAPI/Migrations/20251010104415_AddCountryCodeToUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirBnbCloneAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountryCodeToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "AspNetUsers",
+                type: "nvarchar(2)",
+                maxLength: 2,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "AspNetUsers");
+        }
+    }
+}
