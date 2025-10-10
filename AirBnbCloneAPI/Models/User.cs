cbad43776890e6eb930fd4bcb601448d13165e89
ShroukOuda <s36568961@gmail.com>
2025-10-10 15:19:34 +0300

    add countrycode to user

--- AirBnbCloneAPI/Models/User.cs
+++ AirBnbCloneAPI/Models/User.cs
@@ -9,14 +9,23 @@ public class User : IdentityUser
     [Required]
     [StringLength(50, MinimumLength = 3, ErrorMessage = "First name must be between 3 and 50 characters")]
     public string FirstName { get; set; }
+    
     [Required]
     [StringLength(50, MinimumLength = 3, ErrorMessage = "Last name must be between 3 and 50 characters")]
     public string LastName { get; set; }
+    
+    [Required]
     [DataType(DataType.Date)]
     [Column(TypeName = "date")]
     public DateTime DateOfBirth { get; set; }
+    
+    [Required]
+    [StringLength(2)]
+    public string CountryCode { get; set; }
+    
     [Column(TypeName = "datetime2")]
     public DateTime CreatedAt { get; set; }
+    
     [Column(TypeName = "datetime2")]
     public DateTime? UpdatedAt { get; set; }
 }
